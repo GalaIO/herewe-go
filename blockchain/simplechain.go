b9dbce555626ca5e4d9751805dd00c3196822cf0
gyj176383 <gyj176383@alibaba-inc.com>
2019-03-06 00:27:43 +0800

    增加pow挖矿机制

--- blockchain/simplechain.go
+++ blockchain/simplechain.go
@@ -6,6 +6,8 @@ import (
 	"time"
 	"bytes"
 	"fmt"
+	"strconv"
+	"encoding/base64"
 )
 
 type Block struct {
@@ -14,31 +16,44 @@ type Block struct {
 	height int
 	timestamp int64
 	data []byte
+	// 增肌hash时 随机数用于pow
+	nonce int
 }
 
 // 创建一个新区块
 func New(height int, timestamp int64, data []byte, previousHash []byte) Block {
-	return Block{
+	// 初始随机值
+	nonce := 0
+	block := Block{
 		previousHash,
-		BlockHash256(previousHash, data, timestamp),
+		nil,
 		height,
 		timestamp,
 		data,
+		nonce,
 	}
+	// 挖矿，保证验证一致，直至等到网络出块
+	nonce, hash := block.mineBlock(nonce, 6)
+	block.nonce = nonce
+	block.hash = hash
+	return block
 
 }
 
 func (block *Block) String() string {
-	return fmt.Sprintf("height: %d, data size: %d, time: %s", block.height, len(block.data), time.Unix(block.timestamp, 0).String())
+	return fmt.Sprintf("height: %d, data size: %d, time: %s, nonce: %d, hash: %s", block.height, len(block.data), time.Unix(block.timestamp, 0).String(),
+		block.nonce, strconv.Itoa(int(block.hash[0])) + "||" + base64.StdEncoding.EncodeToString(block.hash))
 }
 
 // 将区块 取hash摘要
-func BlockHash256(previousHash []byte, data []byte, timestamp int64) []byte {
+func BlockHash256(previousHash []byte, data []byte, nonce int, timestamp int64) []byte {
 	instance := sha256.New()
 	instance.Write(previousHash)
 	instance.Write(data)
 	var buf = make([]byte, 8)
 	binary.BigEndian.PutUint64(buf, uint64(timestamp))
+	instance.Write(buf)
+	binary.BigEndian.PutUint64(buf, uint64(nonce))
 	instance.Write(buf)
 	return instance.Sum(nil)
 }
@@ -71,7 +86,7 @@ func (chain *BlockChain) addBlock(data []byte) {
 func (chain *BlockChain) isValidChain() {
 	for i, block := range chain.block {
 		// 验证当前区块的hash
-		tmpHash := BlockHash256(block.previousHash, block.data, block.timestamp)
+		tmpHash := BlockHash256(block.previousHash, block.data, block.nonce, block.timestamp)
 		if !bytes.Equal(tmpHash, block.hash) {
 			fmt.Println(fmt.Sprintf("block hash err, block=%s", block.String()))
 			return
@@ -88,29 +103,69 @@ func (chain *BlockChain) isValidChain() {
 	}
 }
 
+// 挖矿程序，pow机制，前n位为0，简单期间n必须整除4
+func (block *Block) mineBlock(nonce int, difficulty int) (int, []byte) {
+	hashed := BlockHash256(block.previousHash, block.data, nonce, block.timestamp)
+	for !validMinedBlockHash(hashed, difficulty) {
+		nonce ++
+		hashed = BlockHash256(block.previousHash, block.data, nonce, block.timestamp)
+	}
+	return nonce, hashed
+}
+
+// 验证hash是否满足前difficulty位 都是0
+var zeroBytes = [8]byte{
+	0x00,
+	0x7F,
+	0x3F,
+	0x1F,
+	0x0F,
+	0x07,
+	0x03,
+	0x01,
+}
+func validMinedBlockHash(hashed []byte, difficulty int) bool {
+	left := difficulty % 8
+	allZeroCount := difficulty / 8
+	for i, cbyte := range hashed {
+		if i >= allZeroCount {
+			break
+		}
+		if cbyte != 0x00 {
+			return false
+		}
+	}
+	if left <= 0 {
+		return true
+	}
+	return hashed[allZeroCount] & (^zeroBytes[left]) == 0x00
+}
+
 // 生成创世区块，创世区块 没有指向前区块的hash，默认为0，在创世区块一般分配好初始余额 
 func genGenesisBlock() Block {
 	return New(0, time.Now().Unix(), []byte("hello world"), []byte{31})
 }
 
 func main() {
+	//fmt.Println(validMinedBlockHash([]byte{0x00, 0x31}, 10))
+	sampleBlock()
+}
+
+func sampleBlock() {
 	var chain BlockChain
 	chain.initChain()
 	chain.addBlock([]byte("second"))
 	chain.addBlock([]byte("third"))
 	chain.isValidChain()
-
 	//// 篡改区块内容
 	//chain.block[1].data = []byte("fake")
 	//chain.isValidChain()
-
 	//// 篡改 重新计算hash
 	//chain.block[1] = New(1, time.Now().Unix(), []byte("second2"), chain.block[0].hash)
 	//chain.isValidChain()
-
 	// 篡改 重新计算该区块及其后的所有hash
-	chain.block[1] = New(1, time.Now().Unix(), []byte("second2"), chain.block[0].hash)
-	chain.block[2].previousHash = chain.block[1].hash
-	chain.block[2].hash = BlockHash256(chain.block[2].previousHash, chain.block[2].data, chain.block[2].timestamp)
-	chain.isValidChain()
+	//chain.block[1] = New(1, time.Now().Unix(), []byte("second2"), chain.block[0].hash)
+	//chain.block[2].previousHash = chain.block[1].hash
+	//chain.block[2].hash = BlockHash256(chain.block[2].previousHash, chain.block[2].data, chain.block[2].nonce, chain.block[2].timestamp)
+	//chain.isValidChain()
 }
