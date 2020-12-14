bcbe2b23226854eea436f5fcb612c018fb1113e3
GalaIO <505421435@qq.com>
2020-12-14 21:23:23 +0800

    增加下载统计信息

--- crawler2/fetchShiKeyResources.go
+++ crawler2/fetchShiKeyResources.go
@@ -15,25 +15,32 @@ import (
 )
 
 var whiteNameList = map[string]string{
-	"58 重学前端/":             "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/58%20%E9%87%8D%E5%AD%A6%E5%89%8D%E7%AB%AF/",
-	"60 软件工程之美/":           "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/60%20%E8%BD%AF%E4%BB%B6%E5%B7%A5%E7%A8%8B%E4%B9%8B%E7%BE%8E/",
-	"59 程序员进阶攻略（61讲）/":     "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/59%20%E7%A8%8B%E5%BA%8F%E5%91%98%E8%BF%9B%E9%98%B6%E6%94%BB%E7%95%A5%EF%BC%8861%E8%AE%B2%EF%BC%89/",
-	"53Go语言从入门到实战/":        "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/53Go%E8%AF%AD%E8%A8%80%E4%BB%8E%E5%85%A5%E9%97%A8%E5%88%B0%E5%AE%9E%E6%88%98/",
-	"02 杨晓峰-.java核心技术36讲/": "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/02%20%E6%9D%A8%E6%99%93%E5%B3%B0-.java%E6%A0%B8%E5%BF%83%E6%8A%80%E6%9C%AF36%E8%AE%B2/",
-	"03 深入浅出区块链/":          "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/03%20%E6%B7%B1%E5%85%A5%E6%B5%85%E5%87%BA%E5%8C%BA%E5%9D%97%E9%93%BE/",
-	"04-白话法律42讲（完结）/":      "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/04-%E7%99%BD%E8%AF%9D%E6%B3%95%E5%BE%8B42%E8%AE%B2%EF%BC%88%E5%AE%8C%E7%BB%93%EF%BC%89/",
-	//"05 从零开始学架构/":          "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/05%20%E4%BB%8E%E9%9B%B6%E5%BC%80%E5%A7%8B%E5%AD%A6%E6%9E%B6%E6%9E%84/",
-	//"06 9小时搞定微信小程序开发（完结）/": "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/06%209%E5%B0%8F%E6%97%B6%E6%90%9E%E5%AE%9A%E5%BE%AE%E4%BF%A1%E5%B0%8F%E7%A8%8B%E5%BA%8F%E5%BC%80%E5%8F%91%EF%BC%88%E5%AE%8C%E7%BB%93%EF%BC%89/",
-	//"07 微服务架构核心20讲/":       "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/07%20%E5%BE%AE%E6%9C%8D%E5%8A%A1%E6%9E%B6%E6%9E%84%E6%A0%B8%E5%BF%8320%E8%AE%B2/",
+	"06 9小时搞定微信小程序开发（完结）/": "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/06%209%E5%B0%8F%E6%97%B6%E6%90%9E%E5%AE%9A%E5%BE%AE%E4%BF%A1%E5%B0%8F%E7%A8%8B%E5%BA%8F%E5%BC%80%E5%8F%91%EF%BC%88%E5%AE%8C%E7%BB%93%EF%BC%89/",
+	"05 从零开始学架构/":          "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/05%20%E4%BB%8E%E9%9B%B6%E5%BC%80%E5%A7%8B%E5%AD%A6%E6%9E%B6%E6%9E%84/",
+	"30软件测试52讲/":           "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/30%E8%BD%AF%E4%BB%B6%E6%B5%8B%E8%AF%9552%E8%AE%B2/",
+	"29趣谈网络协议音频修复版/":       "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/29%E8%B6%A3%E8%B0%88%E7%BD%91%E7%BB%9C%E5%8D%8F%E8%AE%AE%E9%9F%B3%E9%A2%91%E4%BF%AE%E5%A4%8D%E7%89%88/",
+	"56 程序员工作法":            "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/56%20%E7%A8%8B%E5%BA%8F%E5%91%98%E5%B7%A5%E4%BD%9C%E6%B3%95/",
+	"54 技术领导力300讲-TGO鲲鹏会":  "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/54%20%E6%8A%80%E6%9C%AF%E9%A2%86%E5%AF%BC%E5%8A%9B300%E8%AE%B2-TGO%E9%B2%B2%E9%B9%8F%E4%BC%9A/html/",
+	"18 朱赟技术管理课":           "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/18%20%E6%9C%B1%E8%B5%9F%E6%8A%80%E6%9C%AF%E7%AE%A1%E7%90%86%E8%AF%BE/",
+	"41秒杀学习资料":             "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/41%E7%A7%92%E6%9D%80%E5%AD%A6%E4%B9%A0%E8%B5%84%E6%96%99/",
+	"57 面试现场":              "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/57%20%E9%9D%A2%E8%AF%95%E7%8E%B0%E5%9C%BA/",
 	//"11 大咖说/":              "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/11%20%E5%A4%A7%E5%92%96%E8%AF%B4/",
+
+	// downloaded
+	//"07 微服务架构核心20讲/":       "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/07%20%E5%BE%AE%E6%9C%8D%E5%8A%A1%E6%9E%B6%E6%9E%84%E6%A0%B8%E5%BF%8320%E8%AE%B2/",
 	//"14 深入浅出gRPC-李林峰/":     "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/14%20%E6%B7%B1%E5%85%A5%E6%B5%85%E5%87%BAgRPC-%E6%9D%8E%E6%9E%97%E5%B3%B0/",
 	//"17 持续交付36讲/":          "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/17%20%E6%8C%81%E7%BB%AD%E4%BA%A4%E4%BB%9836%E8%AE%B2/",
 	//"20 如何设计一个秒杀系统-许令波/":   "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/20%20%E5%A6%82%E4%BD%95%E8%AE%BE%E8%AE%A1%E4%B8%80%E4%B8%AA%E7%A7%92%E6%9D%80%E7%B3%BB%E7%BB%9F-%E8%AE%B8%E4%BB%A4%E6%B3%A2/",
 	//"23 技术与商业案例解读-徐飞/":     "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/23%20%E6%8A%80%E6%9C%AF%E4%B8%8E%E5%95%86%E4%B8%9A%E6%A1%88%E4%BE%8B%E8%A7%A3%E8%AF%BB-%E5%BE%90%E9%A3%9E/",
-	//"29趣谈网络协议音频修复版/":       "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/29%E8%B6%A3%E8%B0%88%E7%BD%91%E7%BB%9C%E5%8D%8F%E8%AE%AE%E9%9F%B3%E9%A2%91%E4%BF%AE%E5%A4%8D%E7%89%88/",
-	//"30软件测试52讲/":           "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/30%E8%BD%AF%E4%BB%B6%E6%B5%8B%E8%AF%9552%E8%AE%B2/",
-
-	// downloaded
+	//"12 机器学习40讲-王天一":       "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/12%20%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A040%E8%AE%B2-%E7%8E%8B%E5%A4%A9%E4%B8%80/",
+	//"50 Linux性能优化实战":       "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/50%20Linux%E6%80%A7%E8%83%BD%E4%BC%98%E5%8C%96%E5%AE%9E%E6%88%98/",
+	//"58 重学前端/":             "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/58%20%E9%87%8D%E5%AD%A6%E5%89%8D%E7%AB%AF/",
+	//"60 软件工程之美/":           "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/60%20%E8%BD%AF%E4%BB%B6%E5%B7%A5%E7%A8%8B%E4%B9%8B%E7%BE%8E/",
+	//"59 程序员进阶攻略（61讲）/":     "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/59%20%E7%A8%8B%E5%BA%8F%E5%91%98%E8%BF%9B%E9%98%B6%E6%94%BB%E7%95%A5%EF%BC%8861%E8%AE%B2%EF%BC%89/",
+	//"53Go语言从入门到实战/":        "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/53Go%E8%AF%AD%E8%A8%80%E4%BB%8E%E5%85%A5%E9%97%A8%E5%88%B0%E5%AE%9E%E6%88%98/",
+	//"02 杨晓峰-.java核心技术36讲/": "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/02%20%E6%9D%A8%E6%99%93%E5%B3%B0-.java%E6%A0%B8%E5%BF%83%E6%8A%80%E6%9C%AF36%E8%AE%B2/",
+	//"03 深入浅出区块链/":          "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/03%20%E6%B7%B1%E5%85%A5%E6%B5%85%E5%87%BA%E5%8C%BA%E5%9D%97%E9%93%BE/",
+	//"04-白话法律42讲（完结）/":      "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/04-%E7%99%BD%E8%AF%9D%E6%B3%95%E5%BE%8B42%E8%AE%B2%EF%BC%88%E5%AE%8C%E7%BB%93%EF%BC%89/",
 	//"38微服务架构实战160讲/":       "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/38%E5%BE%AE%E6%9C%8D%E5%8A%A1%E6%9E%B6%E6%9E%84%E5%AE%9E%E6%88%98160%E8%AE%B2/",
 	//"35Go语言核心36讲/": "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/35Go%E8%AF%AD%E8%A8%80%E6%A0%B8%E5%BF%8336%E8%AE%B2/",
 	//"46 数据结构与算法之美/": "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/46%20%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84%E4%B8%8E%E7%AE%97%E6%B3%95%E4%B9%8B%E7%BE%8E/",
@@ -69,7 +76,7 @@ func main() {
 	go dirParseLoop()
 
 	// run 3 goroutinue to download files
-	for i := 0; i < 4; i++ {
+	for i := 0; i < 3; i++ {
 		go downloadFileLoop()
 	}
 
@@ -87,6 +94,7 @@ func fetchPathInit() {
 	//pushDirParse("/Users/galaio/Documentss/ziliao", "https://d.shikey.com/jike/%E6%9E%81%E5%AE%A2%E6%97%B6%E9%97%B4%E5%B7%B2%E5%AE%8C%E7%BB%93/")
 	for key, val := range whiteNameList {
 		pushDirParse("/Users/galaio/Documentss/ziliao/"+key, val)
+		fetchDirStat.IncrTotal()
 	}
 
 	//args := os.Args
@@ -135,9 +143,11 @@ func dirParseLoop() {
 			if err != nil {
 				fmt.Println("parse dir err, url: ", ri.URL, err)
 				pushDirParse(ri.SaveDir, ri.URL)
+			} else {
+				fetchDirStat.IncrFetched()
 			}
 		}()
-		time.Sleep(time.Millisecond * 1000)
+		time.Sleep(time.Millisecond * 2000)
 	}
 }
 func pushDirParse(saveDir, requestPath string) {
@@ -158,9 +168,10 @@ func downloadFileLoop() {
 		if err != nil {
 			fmt.Println("download file err, url: ", ri.URL, err)
 			pushDownloadFile(ri.SaveDir, ri.URL, ri.Name)
+		} else {
+			// stat download info
+			fetchFilesStat.IncrFetched()
 		}
-		// stat download info
-		incrDownloadedFileCount()
 		time.Sleep(time.Millisecond * 1000)
 	}
 }
@@ -194,6 +205,11 @@ func fetchFromUrl(saveDir, requestPath string) error {
 		return err
 	}
 	defer resp.Body.Close()
+	fmt.Println("fetch dir:", requestPath, "resp:", resp.Status, "length:", resp.ContentLength)
+	// Check server response
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
 
 	bytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -206,11 +222,12 @@ func fetchFromUrl(saveDir, requestPath string) error {
 		newUrl := "https://d.shikey.com" + match[1]
 		if strings.HasSuffix(name, "/") {
 			pushDirParse(path.Join(saveDir, name), newUrl)
+			fetchDirStat.IncrTotal()
 			continue
 		}
 		pushDownloadFile(newUrl, saveDir, name)
 		// stat download info
-		incrDownloadFileTotal()
+		fetchFilesStat.IncrTotal()
 	}
 
 	return nil
@@ -250,8 +267,9 @@ func downloadFile(url, dir, name string) error {
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("bad status: %s", resp.Status)
 	}
-	if _, err := os.Stat(newPath); os.IsNotExist(err) {
-		os.Remove(newPath)
+	if _, err := os.Stat(newPath); os.IsExist(err) {
+		fmt.Println("fetch:", newPath, "file existed, skip")
+		return nil
 	}
 	out, err := os.Create(newPath)
 	if err != nil {
@@ -268,20 +286,43 @@ func downloadFile(url, dir, name string) error {
 	return nil
 }
 
-var downloadFileTotal int32
-var downloadedFileCount int32
-
-func incrDownloadFileTotal() {
-	atomic.AddInt32(&downloadFileTotal, 1)
+// stat modules
+type FetchStatInfo struct {
+	Total   int32
+	Fetched int32
 }
-func incrDownloadedFileCount() {
-	atomic.AddInt32(&downloadedFileCount, 1)
+
+func (f *FetchStatInfo) Percent() int {
+	if f.Total <= 0 {
+		return 0
+	}
+	return int(float64(f.Fetched) / float64(f.Total) * 100)
+}
+
+func (f *FetchStatInfo) IncrTotal() {
+	atomic.AddInt32(&f.Total, 1)
+}
+
+func (f *FetchStatInfo) IncrFetched() {
+	atomic.AddInt32(&f.Fetched, 1)
+}
+
+// fetch file stat info
+var fetchFilesStat = FetchStatInfo{
+	Total:   0,
+	Fetched: 0,
+}
+
+// fetch dir stat info
+var fetchDirStat = FetchStatInfo{
+	Total:   0,
+	Fetched: 0,
 }
 
 func downloadStat() {
 	for {
-		fmt.Println("download file stat, total:", downloadFileTotal,
-			"downloaded:", downloadedFileCount, "percent:", 1.0*downloadedFileCount/downloadFileTotal*100, "%")
-		time.Sleep(1 * time.Second)
+		fmt.Println("fetch dir stat, total:", fetchDirStat.Total, "fetched:", fetchDirStat.Fetched, "percent:", fetchDirStat.Percent(), "%")
+		fmt.Println("fetch file stat, total:", fetchFilesStat.Total, "fetched:", fetchFilesStat.Fetched, "percent:", fetchFilesStat.Percent(), "%")
+		time.Sleep(10 * time.Second)
 	}
 }
